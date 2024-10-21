48b2bbe152eb075336914bda4313a3c8164d8d29
syedaUroojFatimaa <farhanmona1983@gmail.com>
2024-10-21 16:20:48 +0500

    updated

---
+++ script.ts
@@ -0,0 +1,36 @@
+const form = document.getElementById("resume-form") as HTMLFormElement;
+const resumeDisplayElement = document.getElementById("resume-display") as HTMLDivElement;
+
+form.addEventListener('submit', (event: Event) => {
+    event.preventDefault();
+
+    const name = (document.getElementById('name') as HTMLInputElement).value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const phone = (document.getElementById('phone') as HTMLInputElement).value;
+    const education = (document.getElementById('education') as HTMLTextAreaElement).value;
+    const experience = (document.getElementById('experience') as HTMLTextAreaElement).value;
+    const skills = (document.getElementById('skills') as HTMLTextAreaElement).value;
+
+    const resumeHTML = `
+    <h2>Resume</h2>
+    <h3>Personal Information</h3>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Phone:</strong> ${phone}</p>
+
+    <h3>Education</h3>
+    <p>${education}</p>
+
+    <h3>Experience</h3>
+    <p>${experience}</p>
+
+    <h3>Skills</h3>
+    <p>${skills}</p>
+    `;
+
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
+    } else {
+        console.error('The Resume Display Element is Missing.');
+    }
+});
